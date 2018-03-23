64937614d54e9d857609dcdec92ca90a12961f20
wdavo <wdavos@gmail.com>
2018-03-24 00:16:57 +1100

    Create electron-import.sh

---
+++ electron-import.sh
@@ -0,0 +1,25 @@
+!#/bin/bash
+
+die(){
+
+	exit 1
+}
+
+export HISTIGNORE="expect*";
+
+expect -c "
+	spawn electron-forge import ../
+	expect "continue"
+	send \"y\r\"
+	expect "babel"
+	send \"n\r\"
+	expect "start"
+	send \"y\r\"
+	expect "package"
+	send \"y\r\"
+	expect "make"
+	send \"y\r\"
+	expect eof
+"
+
+export HISTIGNORE=="";
